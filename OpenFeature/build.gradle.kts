0322112260d3ffce8b0b47106badc4d516a2c3ad
Nicklas Lundin <nicklasl@spotify.com>
2023-05-30 14:07:00 +0200

    make sure the plugin runs on the OpenFeature module

--- OpenFeature/build.gradle.kts
+++ OpenFeature/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    id("org.jlleitschuh.gradle.ktlint")
     kotlin("plugin.serialization") version "1.8.10"
 }
 
