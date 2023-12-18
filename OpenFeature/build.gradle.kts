f4ba35d34c19ddc3a7c4dd81ced01689766af4c3
vahidlazio <vahidlazio@gmail.com>
2023-12-18 08:10:32 -0500

    chore: Remove minify false  (#77)
    Signed-off-by: vahid torkaman <vahidlazio@gmail.com>

--- OpenFeature/build.gradle.kts
+++ OpenFeature/build.gradle.kts
@@ -21,15 +21,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
