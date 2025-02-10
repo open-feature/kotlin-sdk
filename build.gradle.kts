a8b5df2691ffa0bfe6a86bdacbd68e95c8eab4bd
Nicklas Lundin <nicklasl@spotify.com>
2025-02-10 08:46:35 +0100

    build: add plugin to validate api binary compatibility (#123)
    Signed-off-by: Nicklas Lundin <nicklasl@spotify.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android").version("1.8.10").apply(false)
     id("org.jlleitschuh.gradle.ktlint").version("11.6.1").apply(true)
     id("io.github.gradle-nexus.publish-plugin").version("2.0.0").apply(true)
+    id("org.jetbrains.kotlinx.binary-compatibility-validator").version("0.17.0").apply(false)
 }
 allprojects {
     extra["groupId"] = "dev.openfeature"
