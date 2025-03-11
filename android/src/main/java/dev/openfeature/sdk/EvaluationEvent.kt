fc0ff3d95403498696d25fdb8a4a4f29ea1b0248
chrfwow <christian.lutnik@dynatrace.com>
2025-03-11 15:50:50 +0100

    feat: add otel compatible telemetry signal (#129)
    Signed-off-by: christian.lutnik <christian.lutnik@dynatrace.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Co-authored-by: Nicklas Lundin <nicklasl@spotify.com>

---
+++ android/src/main/java/dev/openfeature/sdk/EvaluationEvent.kt
@@ -0,0 +1,27 @@
+package dev.openfeature.sdk
+
+/**
+ * OpenTelemetry compatible telemetry signal for flag evaluations. Can be created by calling [createEvaluationEvent].
+ *
+ * See
+ * [TELEMETRY_KEY],
+ * [TELEMETRY_ERROR_CODE],
+ * [TELEMETRY_VARIANT],
+ * [TELEMETRY_CONTEXT_ID],
+ * [TELEMETRY_ERROR_MSG],
+ * [TELEMETRY_REASON],
+ * [TELEMETRY_PROVIDER],
+ * [TELEMETRY_FLAG_SET_ID],
+ * [TELEMETRY_VERSION],
+ * [TELEMETRY_FLAG_META_CONTEXT_ID],
+ * [TELEMETRY_FLAG_META_FLAG_SET_ID],
+ * [TELEMETRY_FLAG_META_VERSION],
+ * [TELEMETRY_BODY] and
+ * [FLAG_EVALUATION_EVENT_NAME]
+ * for attribute and body keys.
+ */
+data class EvaluationEvent(
+    val name: String,
+    val attributes: Map<String, Any?>,
+    val body: Map<String, Any?>
+)
