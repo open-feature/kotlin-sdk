261825fe9167fb02f8167ba18d3e3e858abae882
Bence Hornák <bence.hornak@gmail.com>
2025-05-17 10:43:53 +0200

    Apply the Kotlin Multiplatform plugin
    Signed-off-by: Bence Hornák <bence.hornak@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.library").version("7.4.2").apply(false)
     id("com.android.application").version("7.4.2").apply(false)
-    id("org.jetbrains.kotlin.android").version("1.8.10").apply(false)
+    id("org.jetbrains.kotlin.multiplatform").version("2.1.21").apply(false)
     id("org.jlleitschuh.gradle.ktlint").version("11.6.1").apply(true)
     id("io.github.gradle-nexus.publish-plugin").version("2.0.0").apply(true)
     id("org.jetbrains.kotlinx.binary-compatibility-validator").version("0.17.0").apply(false)
