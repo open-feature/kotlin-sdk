60632171eb830dd266c567c8f078d9936ac3bebf
Bence Hornák <bence.hornak@gmail.com>
2025-05-17 10:48:07 +0200

    Rename 'android' directory to 'shared'
    Signed-off-by: Bence Hornák <bence.hornak@gmail.com>

--- sampleapp/build.gradle.kts
+++ sampleapp/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":android"))
+    implementation(project(":shared"))
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.3.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "OpenFeature"
-include(":android")
+include(":shared")
 include(":sampleapp")
