c4371b9fbcaabdacd3ef594f44a0b90522c206f1
Bence Hornák <bence.hornak@gmail.com>
2025-05-19 22:02:20 +0200

    Upgrade Gradle dependencies
    Signed-off-by: Bence Hornák <bence.hornak@gmail.com>

--- sampleapp/build.gradle.kts
+++ sampleapp/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "dev.openfeature.sdk.sampleapp"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.openfeature.sdk.sampleapp"
@@ -39,9 +40,9 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4"
+        kotlinCompilerExtensionVersion = "2.1.0"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -50,10 +51,10 @@ android {
 
 dependencies {
     implementation(project(":kotlin-sdk"))
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.3.0")
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
