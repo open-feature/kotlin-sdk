3d648ec6294efcca8fa3f7b9adb38c4dc15cc87b
Michael Beemer <beeme1mr@users.noreply.github.com>
2025-08-04 10:54:43 -0400

    fix: update SonarType Central credentials (#155)
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ nexusPublishing {
         sonatype {
             nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-            username = System.getenv("OSSRH_USERNAME")
-            password = System.getenv("OSSRH_PASSWORD")
+            username = System.getenv("CENTRAL_USERNAME")
+            password = System.getenv("CENTRAL_PASSWORD")
         }
     }
 }
