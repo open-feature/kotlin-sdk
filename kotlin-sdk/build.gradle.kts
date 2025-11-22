8487dd6d25a6963185bfb296149e67244750fd8d
penguindan <daniel.seunkim@gmail.com>
2025-11-22 15:22:13 -0800

    Address pr feedback
    Signed-off-by: penguindan <daniel.seunkim@gmail.com>

--- kotlin-sdk/build.gradle.kts
+++ kotlin-sdk/build.gradle.kts
@@ -40,9 +40,15 @@ kotlin {
         }
     }
     linuxX64 {}
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "OpenFeature"
+        }
+    }
     js {
         nodejs {}
         browser {
